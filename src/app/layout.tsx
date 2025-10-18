0a8fbb313165546270434f989d498b0c3f45548e
unknown <kajugadaniels@gmail.com>
2025-10-18 17:29:49 +0200

    New changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Hephaestus",
+    title: "Schoolify",
     description: "School Management System",
 };
 
