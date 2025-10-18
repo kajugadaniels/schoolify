5f222e118eb31bf7c0cff98540e74f3833e960e3
unknown <kajugadaniels@gmail.com>
2025-10-18 21:01:45 +0200

    Modify login

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -28,8 +28,7 @@ const LoginPage = () => {
                     className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
                 >
                     <h1 className="text-xl font-bold flex items-center gap-2">
-                        <Image src="/logo.png" alt="" width={24} height={24} />
-                        Schoolify
+                        <Image src="/logo.png" alt="" width={140} height={140} />
                     </h1>
                     <h2 className="text-gray-400">Sign in to your account</h2>
                     <Clerk.GlobalError className="text-sm text-red-400" />
